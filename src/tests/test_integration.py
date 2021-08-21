300a062072c6161558b5f2eeaf5e610e17815ec8
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-08-21 19:17:01 +0200

    added very first selenium test

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -4,7 +4,8 @@ import json
 from src.git_graph_automation.renderer import renderHtml
 from src.git_graph_automation.logParser import parseJsonOutput
 from src.git_graph_automation.gitCommand import invokeGitLog
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def test_full_rendering_last_10_commits(tmp_path):
     filePath = tmp_path / "test_full_rendering_last_10_commits.html"
@@ -17,10 +18,10 @@ def test_full_rendering_last_10_commits(tmp_path):
     data = json.dumps(parsed) 
     renderHtml(data, filePath)
 
-@pytest.mark.skip(reason="still does not work")
-def test_full_rendering_in_selenium():
-    filePath = "/tmp/test_full_rendering_last_10_commits.html"
-    outputImage = "/tmp/test_full_rendering_last_10_commits.png"
+#@pytest.mark.skip(reason="still does not work")
+def test_full_rendering_in_selenium(tmp_path):
+    filePath = tmp_path / "test_full_rendering_last_10_commits.html"
+    outputImage = tmp_path / "test_full_rendering_last_10_commits.png"
 
     if os.path.exists(filePath):
         os.remove(filePath)
@@ -34,9 +35,9 @@ def test_full_rendering_in_selenium():
     renderHtml(data, filePath)
 
     # get the name in a correct format
-    temp_name = "file://" + filePath
+    temp_name = "file://" + filePath.as_posix()
 
-    driver = webdriver.PhantomJS()
+    driver = webdriver.Chrome(r'C:/temp/chromedriver_win32/chromedriver.exe')
     driver.set_window_size(1024, 768) 
     #driver.get('https://google.com/') # this works fine
     driver.get(temp_name) # passing the file name or htmlString doesn't work...creates a blank png with nothing
