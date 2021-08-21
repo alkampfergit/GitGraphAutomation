ef3ac833125dd3725382e419267fed8ed8a0913f
Gian Maria <alkampfer@nablasoft.com>
2021-08-21 16:02:30 +0000

    added bare selenium support

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -4,6 +4,7 @@ import json
 from src.git_graph_automation.renderer import renderHtml
 from src.git_graph_automation.logParser import parseJsonOutput
 from src.git_graph_automation.gitCommand import invokeGitLog
+from selenium import webdriver 
 
 def test_full_rendering_last_10_commits():
     filePath = "/tmp/test_full_rendering_last_10_commits.html"
@@ -16,5 +17,28 @@ def test_full_rendering_last_10_commits():
     data = json.dumps(parsed) 
     renderHtml(data, filePath)
 
-    
+def test_full_rendering_in_selenium():
+    filePath = "/tmp/test_full_rendering_last_10_commits.html"
+    outputImage = "/tmp/test_full_rendering_last_10_commits.png"
+
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
+    if os.path.exists(outputImage):
+        os.remove(outputImage)
+
+    gitLog = invokeGitLog(10)
+    parsed = parseJsonOutput(gitLog)
+    data = json.dumps(parsed) 
+    renderHtml(data, filePath)
+
+    # get the name in a correct format
+    temp_name = "file://" + filePath
+
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1024, 768) 
+    #driver.get('https://google.com/') # this works fine
+    driver.get(temp_name) # passing the file name or htmlString doesn't work...creates a blank png with nothing
+    driver.save_screenshot(outputImage) 
+    driver.quit()
     
