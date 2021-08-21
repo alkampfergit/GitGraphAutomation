b85eddbd6839a2f17924cc83a110198c11b7fc89
Gian Maria <alkampfer@nablasoft.com>
2021-08-21 11:17:59 +0000

    Some test, everything seems to work in pytest

--- src/git_graph_automation/logParser.py
+++ src/git_graph_automation/logParser.py
@@ -1,5 +1,10 @@
 import json
 
 def parseJsonOutput(jsonData):
+    if not jsonData:
+        return ""
+    # remember that we pass a list of objects, the output of git log parametrized
+    jsonData = jsonData.rstrip(',') # First of all remove trailing quotes
+    jsonData = "[" + jsonData + "]" # Convert into an array
     fullData = json.loads(jsonData)
-    return ""
+    return fullData
--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -1,9 +1,33 @@
+import pytest
+import os
+import json
 from src.git_graph_automation.logParser import parseJsonOutput
 
-def test_branch_parsing():
+def getTestData(dataFileName):
+    with open(f'{os.path.dirname(__file__)}/data/{dataFileName}', 'r') as file:
+        data = file.read()
+    return data;
+
+@pytest.mark.parametrize("dataFileName", [
+    ("single.json"),
+    ("empty.json")
+])
+def test_not_throw_during_parsing(dataFileName ):
     try:
-        parseJsonOutput("")
+        parseJsonOutput(getTestData(dataFileName))
     except Exception as exc:
         assert False, f"An exception is raised parsing a standard empty array {exc}"
+
+def test_parse_single_record():
+    parsed = parseJsonOutput(getTestData("single.json"))
+    assert len(parsed) == 1
+    assert parsed[0][ "hashAbbrev"] == "29c6b7c"
+
+def test_fix_of_refs():
+    parsed = parseJsonOutput(getTestData("single.json"))
+    elem = parsed[0]
+    assert len(elem["refs"]) == 2
+
+
 
 
